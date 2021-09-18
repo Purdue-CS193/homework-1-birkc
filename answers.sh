ba000d8a6381e026a97cf0c9bd4982c34b76650a
birkc <89415298+birkc@users.noreply.github.com>
2021-09-17 21:35:24 -0400

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -4,42 +4,42 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm -r Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv ~/fafsa.jpg ~/Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rm -rf Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv ~/Programs/Age.cpp ~/Programs/C++
 
 # Task 7
-
+cp Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Programs/C
 
 # Task 9
-
+mv ~/Programs/Name.py ~/Programs/Space.py ~/Programs/Python-3
 
 # Task 10
-
+touch ~/Homework/POL141/Essay.txt
 
 # Task 11
-
+pwd
 
 # Task 12
 : '
 Type your vim/nano customizations below:
-
-
-
+syntax on
+set ttyfast
+set number
 '
